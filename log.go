ebf9a950e1deb8e9c34ece16637d7b90cc883c27
taylor <taylor@rtbmb.local>
2018-07-23 18:20:04 -0600

    Renamed struct Logger -> Log

--- log.go
+++ log.go
@@ -17,72 +17,72 @@ const (
 	LEVELERROR
 )
 
-type Logger struct {
+type Log struct {
 	level   Level
 	timeFmt string
 }
 
-func NewLogger(l Level) Logger {
+func NewLog(l Level) Log {
 	timeFmt := "2006-01-02 15:04:05"
-	return Logger{l, timeFmt}
+	return Log{l, timeFmt}
 }
 
-func (l Logger) Trace(format string, v ...interface{}) {
+func (l Log) Trace(format string, v ...interface{}) {
 	if l.level <= LEVELTRACE {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[37m[TRACE]\033[0m", f)
 	}
 }
 
-func (l Logger) Verbose(format string, v ...interface{}) {
+func (l Log) Verbose(format string, v ...interface{}) {
 	if l.level <= LEVELVERBOSE {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[35m[VERBOSE]\033[0m", f)
 	}
 }
 
-func (l Logger) Debug(format string, v ...interface{}) {
+func (l Log) Debug(format string, v ...interface{}) {
 	if l.level <= LEVELDEBUG {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[34m[DEBUG]\033[0m", f)
 	}
 }
 
-func (l Logger) Infoln(v ...interface{}) {
+func (l Log) Infoln(v ...interface{}) {
 	if l.level <= LEVELINFO {
 		f := fmt.Sprintf("%v", v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[32m[INFO]\033[0m", f)
 	}
 }
-func (l Logger) Info(format string, v ...interface{}) {
+func (l Log) Info(format string, v ...interface{}) {
 	if l.level <= LEVELINFO {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[32m[INFO]\033[0m", f)
 	}
 }
 
-func (l Logger) Test(format string, v ...interface{}) {
+func (l Log) Test(format string, v ...interface{}) {
 	if l.level <= LEVELTEST {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[32m[TEST]\033[0m", f)
 	}
 }
 
-func (l Logger) Warn(format string, v ...interface{}) {
+func (l Log) Warn(format string, v ...interface{}) {
 	if l.level <= LEVELWARN {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[33m[WARN]\033[0m", f)
 	}
 }
 
-func (l Logger) Error(format string, v ...interface{}) {
+func (l Log) Error(format string, v ...interface{}) {
 	if l.level <= LEVELERROR {
 		f := fmt.Sprintf(format, v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[31m[ERROR]\033[0m", f)
 	}
 }
 
-func (l Logger) Errorln(v ...interface{}) {
+func (l Log) Errorln(v ...interface{}) {
 	if l.level <= LEVELERROR {
 		f := fmt.Sprintf("%v", v...)
 		fmt.Println(time.Now().Format(l.timeFmt), "\033[31m[ERROR]\033[0m", f)
